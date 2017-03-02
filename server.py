942cd252d685d8c86f8bdccfaea774cc88f9d22b
Brandon Clapp <clapp09@gmail.com>
2017-03-02 17:53:31 -0600

    Wire up button to trigger event.

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import time
 #import RPi.GPIO as GPIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 pin = 12
 #GPIO.setmode(GPIO.BOARD)
@@ -12,11 +12,17 @@ pin = 12
 def index():
     return render_template('index.html')
 
+@app.route('/scripts/<path:path>')
+def send_scripts(path):
+    return send_from_directory('scripts', path)
+
 @app.route("/trigger/", methods=['POST'])
 def trigger():
     #GPIO.output(pin, True)
+    print('Triggered')
     time.sleep(0.5)
     #GPIO.output(pin, False)
+    return ('', 204)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000, debug=False)
