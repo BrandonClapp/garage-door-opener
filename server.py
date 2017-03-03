2f676b06b0ec6c61507ad64e6e1e8920b266b832
Brandon Clapp <Clapp09@gmail.com>
2017-03-02 21:54:01 -0600

    Enable GPIO

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, send_from_directory
 import time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 app = Flask(__name__, static_url_path='')
 
 pin = 12
-#GPIO.setmode(GPIO.BOARD)
-#GPIO.setup(pin, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.OUT)
 
 @app.route("/")
 def index():
@@ -18,10 +18,10 @@ def send_scripts(path):
 
 @app.route("/trigger/", methods=['POST'])
 def trigger():
-    #GPIO.output(pin, True)
+    GPIO.output(pin, True)
     print('Triggered')
     time.sleep(0.5)
-    #GPIO.output(pin, False)
+    GPIO.output(pin, False)
     return ('', 204)
 
 if __name__ == "__main__":
