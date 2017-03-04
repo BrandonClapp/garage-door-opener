8b6d57aafbb87af346d39636a40efdbda303c9ad
Brandon Clapp <brandonclapp@outlook.com>
2017-03-04 10:33:58 -0600

    Create path for apple touch icon.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, send_from_directory
 import time
 import RPi.GPIO as GPIO
@@ -23,6 +24,10 @@ def trigger():
     time.sleep(0.5)
     GPIO.output(pin, False)
     return ('', 204)
+
+@app.route('/apple-touch-icon.png')
+def touchicon():
+    return send_from_directory( os.path.join(app.root_path , 'static' ) , 'apple-touch-icon.png' )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=False)
